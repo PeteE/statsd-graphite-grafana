77c0a6c9b9efaf4d7a65479c85f748de2f8fc9df
Pete Erickson <petee@mindtouch.com>
2014-03-22 13:35:21 -0500

    forgot to finish grafana setup

--- puppet/modules/grafana/manifests/init.pp
+++ puppet/modules/grafana/manifests/init.pp
@@ -1,7 +1,7 @@
 class grafana (
     $install_dir = "/usr/share/grafana",
     $graphite_hostname = "graphite.example.org",
-    $grafana_uri = "stats.example.org", 
+    $grafana_hostname = "stats.example.org", 
 ) {
     vcsrepo { "${install_dir}":
         ensure => present,
@@ -15,5 +15,14 @@ class grafana (
         mode => 0644,
         content => template("grafana/config.js"),
         require => Vcsrepo["${install_dir}"],
+    }
+    file { "/etc/httpd/conf.d/grafana.conf":
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 0644,
+        content => template("grafana/grafana.conf"),
+        require => Vcsrepo["${install_dir}"],
+        notify => Service["httpd"],
     }
 }
